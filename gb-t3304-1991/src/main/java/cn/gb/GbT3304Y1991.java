0319526daa1a7c058719d39cd194ab012eb2b923
shishuihao <1285173409@qq.com>
2021-09-02 20:47:34 +0800

    gb-t3304-1991

---
+++ gb-t3304-1991/src/main/java/cn/gb/GbT3304Y1991.java
@@ -0,0 +1,258 @@
+package cn.gb;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author shishuihao
+ * @version 1.0.0
+ */
+@Getter
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+public enum GbT3304Y1991 {
+    /**
+     * 土家族.
+     */
+    TJ("土家族", "Tujia", "Tujia", "TJ", "15"),
+    /**
+     * 土族.
+     */
+    TU("土族", "Tu", "Tu", "TU", "30"),
+    /**
+     * 门巴族.
+     */
+    MB("门巴族", "Monba", "Monba", "MB", "54"),
+    /**
+     * 水族.
+     */
+    SU("水族", "Sui", "Sui", "SU", "25"),
+    /**
+     * 毛南族.
+     */
+    MN("毛南族", "Maonan", "Maonan", "MN", "36"),
+    /**
+     * 乌孜别克族.
+     */
+    UZ("乌孜别克族", "Uzbek", "Uzbek", "UZ", "43"),
+    /**
+     * 布依族.
+     */
+    BY("布依族", "Buyei", "Buyei", "BY", "09"),
+    /**
+     * 布朗族.
+     */
+    BL("布朗族", "Blang", "Blang", "BL", "34"),
+    /**
+     * 东乡族.
+     */
+    DX("东乡族", "Dongxiang", "Dongxiang", "DX", "26"),
+    /**
+     * 忆佬族.
+     */
+    GL("忆佬族", "Gelao", "Gelao", "GL", "37"),
+    /**
+     * 松佬族.
+     */
+    ML("松佬族", "Mulao", "Mulao", "ML", "32"),
+    /**
+     * 白族.
+     */
+    BA("白族", "Bai", "Bai", "BA", "14"),
+    /**
+     * 汉族.
+     */
+    HA("汉族", "Han", "Han", "HA", "01"),
+    /**
+     * 达斡尔族.
+     */
+    DU("达斡尔族", "Daur", "Daur", "DU", "31"),
+    /**
+     * 回族.
+     */
+    HU("回族", "Hui", "Hui", "HU", "03"),
+    /**
+     * 低族.
+     */
+    VA("低族", "Va", "Va", "VA", "21"),
+    /**
+     * 壮族.
+     */
+    ZH("壮族", "Zhuang", "Zhuang", "ZH", "08"),
+    /**
+     * 羌族.
+     */
+    QI("羌族", "Qiang", "Qiang", "QI", "33"),
+    /**
+     * 阿昌族.
+     */
+    AC("阿昌族", "Achang", "Achang", "AC", "39"),
+    /**
+     * 纳西族.
+     */
+    NX("纳西族", "Naxi", "Naxi", "NX", "27"),
+    /**
+     * 拉枯族.
+     */
+    LH("拉枯族", "Lahu", "Lahu", "LH", "24"),
+    /**
+     * 苗族.
+     */
+    MH("苗族", "Miao", "Miao", "MH", "06"),
+    /**
+     * 侗族.
+     */
+    DO("侗族", "Dong", "Dong", "DO", "12"),
+    /**
+     * 京族.
+     */
+    GI("京族", "Gin", "Gin", "GI", "49"),
+    /**
+     * 柯尔克孜族.
+     */
+    KG("柯尔克孜族", "Kirgiz", "Kirgiz", "KG", "29"),
+    /**
+     * 哈尼族.
+     */
+    HN("哈尼族", "Hani", "Hani", "HN", "16"),
+    /**
+     * 哈萨克族.
+     */
+    KZ("哈萨克族", "Kazak", "Kazak", "KZ", "17"),
+    /**
+     * 保安族.
+     */
+    BN("保安族", "Bonan", "Bonan", "BN", "47"),
+    /**
+     * 俄罗斯族.
+     */
+    RS("俄罗斯族", "Russ", "Russ", "RS", "44"),
+    /**
+     * 独龙族.
+     */
+    DR("独龙族", "Derung", "Derung", "DR", "51"),
+    /**
+     * 怒族.
+     */
+    NU("怒族", "Nu", "Nu", "NU", "42"),
+    /**
+     * 路巴族.
+     */
+    LB("路巴族", "Lhoba", "Lhoba", "LB", "55"),
+    /**
+     * 高山族.
+     */
+    GS("高山族", "Gaoshan", "Gaoshan", "GS", "23"),
+    /**
+     * 基诺族.
+     */
+    JN("基诺族", "Jino", "Jino", "JN", "56"),
+    /**
+     * 鄂伦春族.
+     */
+    OR("鄂伦春族", "Oroqen", "Oroqen", "OR", "52"),
+    /**
+     * 鄂温克族.
+     */
+    EW("鄂温克族", "Ewenki", "Ewenki", "EW", "45"),
+    /**
+     * 维吾尔族.
+     */
+    UG("维吾尔族", "Uygur", "Uygur", "UG", "05"),
+    /**
+     * 塔吉克族.
+     */
+    TA("塔吉克族", "Tajik", "Tajik", "TA", "41"),
+    /**
+     * 塔塔尔族.
+     */
+    TT("塔塔尔族", "Tatar", "Tatar", "TT", "50"),
+    /**
+     * 朝鲜族.
+     */
+    CS("朝鲜族", "Chosen", "Korean", "CS", "10"),
+    /**
+     * 景颇族.
+     */
+    JP("景颇族", "Jingpo", "Jingpo", "JP", "28"),
+    /**
+     * 傣族.
+     */
+    DA("傣族", "Dai", "Dai", "DA", "18"),
+    /**
+     * 傈僳族.
+     */
+    LS("傈僳族", "Lisu", "Lisu", "LS", "20"),
+    /**
+     * 舍族.
+     */
+    SH("舍族", "She", "She", "SH", "22"),
+    /**
+     * 普米族.
+     */
+    PM("普米族", "Pumi", "Pumi", "PM", "40"),
+    /**
+     * 裕固族.
+     */
+    YG("裕固族", "Yugur", "Yugur", "YG", "48"),
+    /**
+     * 蒙古族.
+     */
+    MG("蒙古族", "Mongol", "Mongol", "MG", "02"),
+    /**
+     * 锡伯族.
+     */
+    XB("锡伯族", "Xibe", "Xibe", "XB", "38"),
+    /**
+     * 满族.
+     */
+    MA("满族", "Man", "Man", "MA", "11"),
+    /**
+     * 瑶族.
+     */
+    YA("瑶族", "Yao", "Yao", "YA", "13"),
+    /**
+     * 赫哲族.
+     */
+    HZ("赫哲族", "Hezhen", "Hezhen", "HZ", "53"),
+    /**
+     * 撒拉族.
+     */
+    SL("撒拉族", "Salar", "Salar", "SL", "35"),
+    /**
+     * 德昂族.
+     */
+    DE("德昂族", "Deang", "Deang", "DE", "46"),
+    /**
+     * 黎族.
+     */
+    LI("黎族", "Li", "Li", "LI", "19"),
+    /**
+     * 藏族.
+     */
+    ZA("藏族", "Zang", "Tibetan", "ZA", "04"),
+    /**
+     * 彝族.
+     */
+    YI("彝族", "Yi", "Yi", "YI", "07");
+    /**
+     * 民族名称.
+     */
+    private final String name;
+    /**
+     * 罗马字母拼写法（内部使用）.
+     */
+    private final String romanInside;
+    /**
+     * 罗马字母拼写法（外部使用）.
+     */
+    private final String romanOutside;
+    /**
+     * 字母代码.
+     */
+    private final String code;
+    /**
+     * 数字代码.
+     */
+    private final String number;
+}
